7582e108aea6e258dc1c03c416bec610038a1d80
Ben <benclark001@gmail.com>
2021-12-05 17:58:22 +1100

    add image generation on route

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fabric = require("fabric").fabric
+
 const app = express()
 const port = 3000
 
@@ -13,7 +15,44 @@ app.get('/:width([0-9]+)', (req, res) => {
 
 app.get('/:width([0-9]+)/:height([0-9]+)', (req, res) => {
 	const { width, height } = req.params
-	res.send(`Width ${width}, Height ${height}`)
+	// res.send(`Width ${width}, Height ${height}`)
+
+	var canvas = new fabric.StaticCanvas(null, { width, height })
+
+	fabric.Image.fromURL(`file://${__dirname}/img/1.png`, (img) => {
+
+		// insert image
+		const maxWidth = parseInt(width)
+		const maxHeight = parseInt(height)
+		
+		const aspectRatio = Math.min(maxWidth / img.width, maxHeight / img.height)
+	
+		const scaledWidth = img.width * aspectRatio
+		const scaledHeight = img.height * aspectRatio
+	
+		const left = (maxWidth > scaledWidth) ? ((Math.round(maxWidth) - Math.round(scaledWidth)) / 2) : 0
+		const top = (maxHeight > scaledHeight) ? ((Math.round(maxHeight) - Math.round(scaledHeight)) / 2) : 0
+	
+		img.scale(aspectRatio)
+		img.set({ top, left })
+	
+		canvas.setBackgroundImage(img, () => canvas.renderAll())
+
+		// return image
+		const dataUrl = canvas.toDataURL()
+		
+		canvas.dispose()
+	
+		const regex = /^data:.+\/(.+)base64,(.*)$/
+		const matches = dataUrl.match(regex)
+		const ext = matches[1]
+		const data = matches[2]
+	
+		const buffer = Buffer.from(data, 'base64')
+	
+		res.type('png')
+		res.end(buffer)
+	})
 })
 
 app.listen(port, () => {
